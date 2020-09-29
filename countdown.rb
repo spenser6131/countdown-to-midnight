eb4a25252027fd3b5138cdc0b75fee5d5b4aa21d
spenser6131 <brinkman.spenser@gmail.com>
2020-09-29 13:31:10 -0700

    countdown092920

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+    i -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
